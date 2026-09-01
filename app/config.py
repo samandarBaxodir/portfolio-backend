4549d87ae565666718447bc18aaeebf34f459f4c
samandar <baxodirsamandar7@gmail.com>
2026-09-01 10:15:58 +0500

    Config ga Supabase sozlamalari qo'shildi

--- app/config.py
+++ app/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     cloudinary_cloud_name: str = ""
     cloudinary_api_key: str = ""
     cloudinary_api_secret: str = ""
+    supabase_url: str = ""
+    supabase_key: str = ""
 
     class Config:
         env_file = ".env"
